b23ff2a9615f99cc8026cd1ae3b6e0a5ecf01c63
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-14 15:23:42 +0300

    deploy

---
+++ nginx/gcp2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update -y 
+sudo apt-get install git -y
+snap install docker
+sudo git clone https://github.com/matanshikli/docker_compose.git
+cd docker_compose/
+sudo docker-compose up
