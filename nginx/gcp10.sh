cb491e2b86ded69bbf8f969afba57a1c15768321
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-14 16:59:47 +0300

    deploy

--- nginx/gcp10.sh
+++ nginx/gcp10.sh
@@ -4,6 +4,6 @@ sudo apt-get install git -y
 sudo apt install docker.io -y 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo git clone https://github.com/matanshikli/docker_compose.git /opt/
-sudo docker-compose -f /opt/docker-compose.yml up
+sudo git clone https://github.com/matanshikli/docker_compose.git /opt/project
+sudo docker-compose -f /opt/project/docker-compose.yml up
 echo "done"
