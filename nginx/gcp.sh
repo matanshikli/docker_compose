395e1f6a1f30b12c68df1206b05602b1db78de26
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-14 14:34:20 +0300

    deploy

--- nginx/gcp.sh
+++ nginx/gcp.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-apt-get update -y 
-apt-get install git -y
-apt-get install docker.io -y
-apt-get install docker-compose-plugin -y
+sudo apt-get update -y 
+sudo apt-get install git -y
+sudo apt-get install docker.io -y
+sudo snap install docker -y
+sudo apt-get install docker-compose-plugin -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/matanshikli/docker_compose.git
 cd docker_compose/
 docker-compose -d
