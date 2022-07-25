5a3bb77286389a5acef2e45c2c5fd5770687b1ea
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-25 17:47:50 +0300

    add fw tag

--- scripts/gcp.sh
+++ scripts/gcp.sh
@@ -5,4 +5,4 @@ apt install docker.io -y
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/matanshikli/docker_compose.git /opt/project
-docker-compose -f /opt/project/docker-compose.yml up -d 
+cd /opt/project && docker-compose up -d 
