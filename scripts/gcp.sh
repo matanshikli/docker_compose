5e28de04542003ced1cca0a5bea640ac4d162c13
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-25 16:29:37 +0300

    add 5000 tag to fw

--- scripts/gcp.sh
+++ scripts/gcp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo apt-get update -y 
-sudo apt-get install git -y
-sudo apt install docker.io -y 
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo git clone https://github.com/matanshikli/docker_compose.git /opt/project
-sudo docker-compose -f /opt/project/docker-compose.yml up -d mysql 
-sleep 180
-sudo docker-compose -f /opt/project/docker-compose.yml up
-
-echo "done"
+apt-get update -y 
+apt-get install git -y
+apt install docker.io -y 
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/matanshikli/docker_compose.git /opt/project
+docker-compose -f /opt/project/docker-compose.yml up -d mysql && sleep 50
+echo "SQL is up!" && sleep 40
+echo "creating the rest" && sleep 20
+docker-compose -f /opt/project/docker-compose.yml down
+echo "SQL is down" && sleep 40
+docker-compose -f /opt/project/docker-compose.yml up -d 
