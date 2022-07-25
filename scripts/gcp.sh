23785e583236c7b24b0d60b5e4b3726db56f1111
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-25 17:25:27 +0300

    check

--- scripts/gcp.sh
+++ scripts/gcp.sh
@@ -5,9 +5,4 @@ apt install docker.io -y
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/matanshikli/docker_compose.git /opt/project
-docker-compose -f /opt/project/docker-compose.yml up -d mysql && sleep 50
-echo "SQL is up!" && sleep 40
-echo "creating the rest the the stack" && sleep 20
-docker-compose -f /opt/project/docker-compose.yml down
-echo "SQL is down" && sleep 40
 docker-compose -f /opt/project/docker-compose.yml up -d 
