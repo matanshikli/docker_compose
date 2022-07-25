2eef857d574330eceabfd1af5e7c245ee5db95ca
Matan Shikli <106749791+matanshikli@users.noreply.github.com>
2022-07-25 13:17:06 +0300

    add 5000 tag to fw

--- scripts/gcp.sh
+++ scripts/gcp.sh
@@ -5,5 +5,7 @@ sudo apt install docker.io -y
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo git clone https://github.com/matanshikli/docker_compose.git /opt/project
+sudo docker-compose -f /opt/project/docker-compose.yml up mysql && sleep 45
+sudo docker-compose -f /opt/project/docker-compose.yml down && sleep 15
 sudo docker-compose -f /opt/project/docker-compose.yml up
 echo "done"
